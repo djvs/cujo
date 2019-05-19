f95cd22a2d557993072767d3dd0076c03cc96663
djvs <djvansch@gmail.com>
2019-05-18 22:12:30 -0400

    remove getHeightData

--- src/client/index.js
+++ src/client/index.js
@@ -45,33 +45,6 @@ var app = require("./app")
     }, 3000)
   }
 
-  function getHeightData(img) {
-    var canvas = document.createElement("canvas")
-    canvas.width = 1024
-    canvas.height = 1024
-    var context = canvas.getContext("2d")
-
-    var size = 1024 * 1024,
-      data = new Float32Array(size)
-
-    context.drawImage(img, 0, 0)
-
-    for (var i = 0; i < size; i++) {
-      data[i] = 0
-    }
-
-    var imgd = context.getImageData(0, 0, 1024, 1024)
-    var pix = imgd.data
-
-    var j = 0
-    for (var i = 0, n = pix.length; i < n; i += 4) {
-      var all = pix[i] + pix[i + 1] + pix[i + 2]
-      data[j++] = all / 30
-    }
-
-    return data
-  }
-
   const delableObjs = []
 
   const add = obj => {
